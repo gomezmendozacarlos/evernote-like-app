43aa382f96ac9702d4f3130f84e848604b4b26de
Carlos Gomez <gomezmendozacarlos@gmail.com>
2025-06-11 18:12:28 -0600

    Adds: dependencies to package.json and Fix: styles error

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 import NoteOperations from "@/app/pages/components/NoteOperations";
 import NoteDetails from "@/app/pages/components/NoteDetails";
 import styles from "@/app/styles/Evernote/Evernote.module.scss";
-import "react-quill/dist/quill.snow.css";
+import "react-quill-new/dist/quill.snow.css";
 import "@/app/globals.css";
 
 export default function Home() {
